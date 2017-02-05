12aa6a2fccc2f20de3236302422de096ce31307d
erivansousa <erivan.spe@gmail.com>
2017-02-05 20:52:29 -0300

    primeiro teste de coneccao e consulta no banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,17 @@
 module.exports = function(app){
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host :'localhost',
+            user :'root',
+            password : 'eriv35',
+            database : 'aulanode'
+        });
+
+        connection.query('select * from noticias',function(erro, resultado){
+            //função de callback
+            res.send(resultado);
+        });
+        //res.render("noticias/noticias");
     });
 }
