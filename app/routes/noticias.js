61494a3522fcc31457eb8328d23206838cd56868
erivansousa <erivan.spe@gmail.com>
2017-02-07 05:07:27 -0300

    modularização da conecção com o banco de dados e implementação de busca e exibição dos dados na view

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,17 +1,9 @@
+var connection = require('../../config/dbConnection')();
 module.exports = function(app){
     app.get('/noticias', function(req, res){
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host :'localhost',
-            user :'root',
-            password : 'eriv35',
-            database : 'aulanode'
-        });
-
         connection.query('select * from noticias',function(erro, resultado){
             //função de callback
-            res.send(resultado);
+            res.render("noticias/noticias", {noticias : resultado});
         });
-        //res.render("noticias/noticias");
     });
 }
